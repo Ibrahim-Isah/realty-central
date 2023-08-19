ee9b165246295bcd73b484142aea955a27aeae52
Isah Ibrahim <isahibn08@gmail.com>
2023-08-19 22:22:06 +0100

    added the faqs

---
+++ lib/data.ts
@@ -0,0 +1,56 @@
+export const faqs = [
+	{
+		question: 'What is Realty Central and its mission?',
+		answer: [
+			'Realty Central is a software designed to streamline the work of real estate agents.',
+			'Our mission is to equip real estate professionals with the Real Smart Assistant, a comprehensive tool that combines website marketing, organized listing management, and effective client relationship management. This technology-driven approach aims to increase profits and enhance time management, ensuring success in the competitive real estate market.',
+		],
+	},
+	{
+		question: 'What is the vision of Realty Central?',
+		answer: [
+			'Our vision is to create a world where REALTORS® and real estate agents experience reduced stress and increased satisfaction.',
+			'In this vision, technology serves to improve roles rather than replace them, allowing professionals to streamline business processes and perform routine tasks more effectively.',
+		],
+	},
+	{
+		question: 'What services does Realty Central offer?',
+		answer: [
+			'Efficient Document Management: Simplify document organization and secure access for agents, with advanced search capabilities for swift retrieval.',
+			'Comprehensive Lease Management: Monitor lease agreements, rental payments, and lease expirations seamlessly, with automated reminders for timely actions.',
+			'Effective Customer Relationship Management (CRM): Handle customer data, track interactions, and maintain communication history for personalized property recommendations.',
+			'Efficient Maintenance and Timely Reminders: Manage property upkeep and repairs, aided by AI-driven predictive maintenance for preventive actions.',
+			'Insightful Real-Time Reporting and Analytics: Access real-time dashboards and reports, providing key performance indicators and trend identification.',
+			'Automated Email Communication: Automate email communication for new property listings, rent reminders, and updates, with email analytics for campaign optimization.',
+		],
+	},
+	{
+		question: "How does Realty Central's CRM benefit real estate agents?",
+		answer: [
+			"Realty Central's integrated CRM empowers agents to efficiently handle customer data, track interactions, and maintain detailed communication history.",
+			'The CRM facilitates personalized property recommendations, enhancing client relationships and overall satisfaction.',
+		],
+	},
+	{
+		question: 'How does the maintenance scheduling system work?',
+		answer: [
+			'Our software features a comprehensive maintenance scheduling system that aids in managing property upkeep and repair tasks.',
+			'AI-driven predictive maintenance schedules preventive actions, helping to minimize unexpected repair expenses and disruptions.',
+		],
+	},
+	{
+		question: 'What insights do real-time reporting and analytics provide?',
+		answer: [
+			'Realty Central provides real-time dashboards and reports, offering valuable insights into key performance indicators, property portfolio health, and financial metrics.',
+			'The analytics enable trend identification and pattern recognition, allowing for strategic decision-making and continued success.',
+		],
+	},
+	{
+		question:
+			'How does automated email communication enhance client engagement?',
+		answer: [
+			'Automated email communication from Realty Central keeps clients informed about new property listings, rent reminders, and important updates.',
+			'Email analytics provide insights into open rates and engagement, optimizing email campaigns for maximum impact.',
+		],
+	},
+];
