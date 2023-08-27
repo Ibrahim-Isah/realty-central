4ee89887ce92392b2e5c85aea9fe83d86f33962a
Isah Ibrahim <isahibn08@gmail.com>
2023-08-27 13:04:15 +0100

    adding the authentication

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SignIn } from '@clerk/nextjs';
+
+const Login = () => {
+	return (
+		<div>
+			<SignIn />
+		</div>
+	);
+};
+
+export default Login;
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,25 +1,13 @@
 import '../globals.css';
-import type { Metadata } from 'next';
-import { Poppins } from 'next/font/google';
 
-const poppins = Poppins({
-	weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-	subsets: ['latin'],
-});
-
-export const metadata: Metadata = {
-	title: 'Realty Central',
-	description: 'Realty Central is a real estate management software',
-};
-
-export default function RootLayout({
+export default function DashboardLayout({
 	children,
 }: {
 	children: React.ReactNode;
 }) {
 	return (
-		<html lang='en'>
-			<body className={poppins.className}>{children}</body>
-		</html>
+		<div>
+			<main>{children}</main>
+		</div>
 	);
 }
