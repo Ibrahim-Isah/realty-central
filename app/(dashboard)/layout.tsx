0eac078a0eb27bd8244878dab77dc42f28247025
Isah Ibrahim <isahibn08@gmail.com>
2023-08-29 11:05:43 +0100

    correction on dashboard

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -10,10 +10,14 @@ export default function DashboardLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<div>
+		<>
+			<main className='flex flex-auto lg:pl-[300px] pt-16 max-w-full'>
+				<div className='flex flex-col flex-auto w-full min-h-full mx-2'>
+					{children}
+				</div>
+			</main>
 			<DashboardTopbar />
 			<DashboardSidebar />
-			<main>{children}</main>
-		</div>
+		</>
 	);
 }
